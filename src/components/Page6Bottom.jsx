2464af34c22817e8209940ddcc10d01ac844fe12
John Doe <chougulesanket30@gmail.com>
2025-01-19 11:10:24 +0530

    ios media issue

--- src/components/Page6Bottom.jsx
+++ src/components/Page6Bottom.jsx
@@ -3,60 +3,93 @@ import React from "react";
 const Page6Bottom = () => {
   return (
     <div>
-    <div className="absolute lg:h-[30%] h-[30vh] w-[65vw] md:w-[60vw] lg:w-full lg:bottom-0 -right-9 flex items-center justify-between lg:flex-row flex-col top-[28vh] lg:top-[70vh] sm:top-[78vh]">
-      <div id="description" className="lg:w-1/3 flex  justify-center text-white px-16 font-[anzo3] lg:text-xl text-sm ">
-        <div>
-          <h3>
-            I am a self-taught web and brand designer with a passion for
-            creating unique and timeless masterpieces for ambitious
-            professionals, entrepreneurs, artists, and boutique businesses. ​
-            Bespoke Freelance for agencies.
-          </h3>
-          <div className="flex items-center justify-start mt-12 ">
-          <img className="mr-6 mb-6 h-14" src="https://static.wixstatic.com/media/f1c650_67833facdc7a47d5aae7870d4fb7fc3e~mv2.png/v1/fill/w_85,h_85,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/SUN.png" alt="" />
+      <div className="absolute lg:h-[30%] h-[30vh] w-[65vw] md:w-[60vw] lg:w-full lg:bottom-0 -right-9 flex items-center justify-between lg:flex-row flex-col top-[28vh] lg:top-[70vh] sm:top-[78vh]">
+        <div
+          id="description"
+          className="lg:w-1/3 flex  justify-center text-white px-16 font-[anzo3] lg:text-xl text-sm "
+        >
+          <div>
+            <h3>
+              I am a self-taught web and brand designer with a passion for
+              creating unique and timeless masterpieces for ambitious
+              professionals, entrepreneurs, artists, and boutique businesses. ​
+              Bespoke Freelance for agencies.
+            </h3>
+            <div className="flex items-center justify-start mt-12 ">
+              <img
+                className="mr-6 mb-6 h-14"
+                src="https://static.wixstatic.com/media/f1c650_67833facdc7a47d5aae7870d4fb7fc3e~mv2.png/v1/fill/w_85,h_85,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/SUN.png"
+                alt=""
+              />
 
-            <h1 className="font-[anzo4] text-[5vw]">DESIGN <span className="font-[anzo3] text-xl">01</span></h1>
+              <h1 className="font-[anzo4] text-[5vw]">
+                DESIGN <span className="font-[anzo3] text-xl">01</span>
+              </h1>
+            </div>
           </div>
-        </div>
-        
-        <div></div>
-      </div>
-      <div id="description" className="lg:w-1/3 flex items-center justify-center text-white px-16 font-[anzo3] lg:text-xl text-sm">
-        <div>
-          <h3>
-          With a background in strategic marketing and acknowledged expertise in web development, I aim to minimize my client's resources spent on macro and micro-management, marketing research, and hiring a whole team of a larger agency.
-          </h3>
-          <div className="flex items-center justify-start mt-8 ">
-          <img className="mr-6 mb-6 h-14" src="https://static.wixstatic.com/media/f1c650_027515148725478cbb3a91eca452cabf~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/EARTH.png" alt="" />
 
-            <h1 className="font-[anzo4] text-[5vw]">DEVELOPMENT <span className="font-[anzo3] text-xl">02</span></h1>
+          <div></div>
+        </div>
+        <div
+          id="description"
+          className="lg:w-1/3 flex items-center justify-center text-white px-16 font-[anzo3] lg:text-xl text-sm"
+        >
+          <div>
+            <h3>
+              With a background in strategic marketing and acknowledged
+              expertise in web development, I aim to minimize my client's
+              resources spent on macro and micro-management, marketing research.
+            </h3>
+            <div className="flex items-center justify-start mt-8 ">
+              <img
+                className="mr-6 mb-6 h-14"
+                src="https://static.wixstatic.com/media/f1c650_027515148725478cbb3a91eca452cabf~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/EARTH.png"
+                alt=""
+              />
+
+              <h1 className="font-[anzo4] text-[5vw]">
+                DEVELOPMENT <span className="font-[anzo3] text-xl">02</span>
+              </h1>
+            </div>
           </div>
-        </div>
-        
-        <div></div>
-      </div>
-      <div id="description" className="lg:w-1/3 flex items-center justify-center text-white px-16 font-[anzo3] lg:text-xl text-sm ">
-        <div>
-          <h3>
-          My mission is to drive exceptional growth for my clients by increasing their brand appearance, defining their identity, and engaging customers through captivating black-and-white aesthetics with a modern, luxurious, and minimalistic touch
-          </h3>
-          <div className="flex items-center justify-start mt-8 ">
-          <img className="mr-6 mb-6 h-14" src="https://static.wixstatic.com/media/f1c650_b0000fd9a706421aa0ea84da62ae3354~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/START%20UP.png" alt="" />
 
-            <h1 className="font-[anzo4] text-[5vw]">BRANDING <span className="font-[anzo3] text-xl">03</span></h1>
+          <div></div>
+        </div>
+        <div
+          id="description"
+          className="lg:w-1/3 flex items-center justify-center text-white px-16 font-[anzo3] lg:text-xl text-sm "
+        >
+          <div>
+            <h3>
+              My mission is to drive exceptional growth for my clients by
+              increasing their brand appearance, defining their identity, and
+              engaging customers through captivating black-and-white aesthetics
+              with a modern, luxurious, and minimalistic touch
+            </h3>
+            <div className="flex items-center justify-start mt-8 ">
+              <img
+                className="mr-6 mb-6 h-14"
+                src="https://static.wixstatic.com/media/f1c650_b0000fd9a706421aa0ea84da62ae3354~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/START%20UP.png"
+                alt=""
+              />
+
+              <h1 className="font-[anzo4] text-[5vw]">
+                BRANDING <span className="font-[anzo3] text-xl">03</span>
+              </h1>
+            </div>
           </div>
-        </div>
-        
-        <div></div>
-      </div>
 
-     </div>
-     <div  className="text-gray-400 absolute bottom-10 ml-14 text-sm lg:text-xl">
+          <div></div>
+        </div>
+      </div>
+      <div className="text-gray-400 absolute bottom-10 ml-14 text-sm lg:text-xl">
         <h1>GOT A PROJECT?</h1>
-        <h1>THEN PLEASE <span className="text-white font-[anzo1]">CONTACT ME</span>  </h1>
-        </div>
+        <h1>
+          THEN PLEASE{" "}
+          <span className="text-white font-[anzo1]">CONTACT ME</span>{" "}
+        </h1>
+      </div>
     </div>
-
   );
 };
 
