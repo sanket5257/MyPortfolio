9a8b75bcc4987fe8c34cbb7760e70d9e462486d9
John Doe <chougulesanket30@gmail.com>
2025-01-20 19:37:43 +0530

    techstack

--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -15,46 +15,27 @@ function TechStack() {
     'MySQL.png',
     'Java.png',
     'Mongo.png',
-    'Node.png'
+    'Node.png',
   ];
 
-  useGSAP(function(){
-     // GSAP ScrollTrigger animation for images
-    const images = document.querySelectorAll('.tech-image');
-
-    images.forEach((image) => {
-      gsap.from(image, {
-        opacity: 0,
-        y: -100, // Start from 100px above
-        stagger: 0.2, // Stagger the animation by 0.2 seconds
-        duration: 1, // Duration of the animation
-        ease: 'expo.out', // Smooth easing effect
-        scrollTrigger: {
-          trigger: image, // Trigger the animation when the image enters the viewport
-          start: 'top 70%', // Start the animation when the top of the image is at 80% from the top of the viewport
-          end: 'bottom 50%', // End when the bottom of the image is at 20% from the bottom of the viewport
-          toggleActions: 'play none none none', // Play the animation only once
-          scrub: true, // Optional: makes the animation smooth while scrolling
-        },
-      });
-    });
-  })
-    
   
+   
+    
+    
 
   return (
     <div
       id="TechStack"
-      className="bg-white text-black dark:bg-slate-900 dark:text-white p-8 lg:p-20 flex flex-col items-center justify-center"
+      className=" w-full text-black dark:bg-slate-900 dark:text-white p-8 lg:p-20 flex flex-col items-center justify-center"
     >
-      <h1 className="text-4xl lg:text-5xl font-semibold mb-12 leading-normal uppercase text-gray-500 font-[anzo4]">
+      <h1 className="text-[18vw] xl:text-[18vw] font-semibold mb-12 leading-normal uppercase text-gray-500 font-[anzo4]">
         Tech Stack
       </h1>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8">
         {techStackImages.map((image, index) => (
           <div
             key={index}
-            className="flex items-center justify-center bg-gray-300 dark:bg-fuchsia-800 p-6 rounded-xl border-2 border-black shadow-lg transform transition-all hover:scale-105 tech-image"
+            className="flex items-center justify-center bg-white dark:bg-fuchsia-800 p-6 rounded-xl border-3 border-black shadow-lg transform transition-all animate-bounce tech-image"
           >
             <img
               className="h-24 md:h-28 lg:h-36"
