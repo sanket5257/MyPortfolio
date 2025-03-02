055e2d25880b2144557ef9497d349051926a69ba
John Doe <chougulesanket30@gmail.com>
2025-03-02 19:22:08 +0530

    about added

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -14,11 +14,14 @@ const Aboutme = () => {
         ></video>
         <div className="absolute top-10 w-full h-full text-9xl md:text-[24rem] px-16  ">
           <h1 className="font-[anzo4] uppercase text-white ">[about]</h1>
-          <p className="text-white text-xl md:text-3xl font-bold">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
-            maxime temporibus sunt maiores quae cupiditate repellendus, non
-            asperiores repellat nihil nesciunt illum, quam in aliquam. Itaque
-            non ab consequuntur vero?
+          <p className="text-white text-[3.5vw] md:text-[1.5vw] font-bold">
+          Hello! I'm Sanket, a final-year B.Tech student specializing in Computer Science and Engineering. I am passionate about technology, and I focus on creating modern, visually appealing, and user-friendly web designs.
+
+As a frontend web developer, I have a deep interest in crafting clean, responsive, and interactive web applications. I bring designs to life by using the latest web technologies such as HTML, CSS, JavaScript, and frameworks like React. My work reflects a blend of functionality and creativity, with a particular emphasis on animations and smooth user experiences.
+
+I am always eager to learn new tools and techniques to stay on top of the ever-evolving field of web development. In addition to my academic background, I love experimenting with creative design ideas and incorporating them into my projects.
+
+Feel free to explore my portfolio and connect with me!
           </p>
         </div>
       </div>
