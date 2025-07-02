39e92c74a00de8e346e29bd0a6a6e79e153e6025
sanket <chougulesanket30@gmail.com>
2025-07-02 12:16:19 +0530

    remove framer-motion dependency and related components; delete unused assets and clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,11 @@ import Header from './components/Header';
 import Page2 from './pages/Page2';
 import HireMe from './pages/HireMe';
 import { Route, Routes } from 'react-router-dom';
-import Work from './pages/Work';
 import Page3 from './pages/Page3';
 import Page4 from './pages/Page4';
 import Page5 from './pages/Page5';
 import Page6 from './pages/Page6';
-import About from './pages/About';
 import Aboutme from './pages/Aboutme';
-import Projects from './components/Projects';
 
 
 
@@ -29,7 +26,6 @@ const App = () => {
           
 
           <Routes>
-          <Route path='/projects' element={<Projects/>} />
         <Route path='/HireMe' element={<HireMe/>} />
         
         <Route path='/' element={
